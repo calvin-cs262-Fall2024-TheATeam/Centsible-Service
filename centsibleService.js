81959965a317fde44f4f58c1f1cbaeed2e19f743
Alina086 <as86@calvin.edu>
2024-11-30 02:29:51 -0500

    budgetAPI

--- centsibleService.js
+++ centsibleService.js
@@ -29,6 +29,14 @@ router.delete('/transactions/:id', deleteTransaction);
 router.get('/currentBalance/:id', readCurrentBalance);
 router.put('/currentBalance/:id', updateCurrentBalance);
 
+router.post('/defaultMonthBudget', createDefaultMonthBudget);
+router.get('/monthBudget', readMonthBudget);
+router.put('/monthBudget', updateMonthBudget);
+router.post('/budgetSubcategory', createSubcategory);
+router.put('/budgetSubcategoryAmount', updateSubcategoryAmount);
+router.put('/budgetSubcategoryName', updateSubcategoryName);
+router.delete('/budgetSubcategory/:id', deleteSubcategory);
+
 app.use(router);
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -76,7 +84,7 @@ function readTransactions(req, res, next) {
 
 // Delete Transaction of a User
 function deleteTransaction(req, res, next) {
-  db.oneOrNone('DELETE FROM TransactionEntry WHERE ID=${id}', req.params)
+  db.oneOrNone('DELETE FROM TransactionEntry WHERE ID=${id} RETURNING id', req.params)
     .then((data) => {
       returnDataOr404(res, data);
     })
@@ -109,6 +117,108 @@ function updateCurrentBalance(req, res, next) {
       next(err);
     });
 }
+
+
+// Create default month budget (ie. default categories for a month with default dollar amounts of $0) for a User
+// Note: client provides appuserID, month and year
+function createDefaultMonthBudget(req, res, next) {
+  const categoryList = ['Housing', 'Entertainment', 'Personal', 'Food', 'Transportation'];
+  const { appuserID, month, year } = req.body;
+
+  // insertValues is a list of 5 objects with each having the following attributes
+  const insertValues = categoryList.map(category => ({
+    appuserID,
+    categoryname: category,
+    monthlydollaramount: 0,
+    month,
+    year
+  }));
+
+  // this ForLoop inserts five rows of default budget categories
+  for (const value of insertValues) {
+    db.none("INSERT INTO BudgetCategory(appuserID, categoryname, monthlydollaramount, month_, year_) VALUES (${appuserID}, ${category}, ${monthlydollaramount}, ${month}, ${year});", value)
+      .then(() => {
+        res.sendStatus(201); // Send a 201 Created status code
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+}
+
+
+// Gets budget information of a User for a particular month
+function readMonthBudget(req, res, next) {
+  db.many('SELECT * FROM BudgetCategory WHERE appuserID=${appuserID} AND month_=${month} AND year_=${year}', req.body)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+
+// Updates the monthly dollar amount of a budget category
+// Note: 'monthlydollaramount' should be computed on the client side by totalling up the amounts of the subcategories, if they exist
+function updateMonthBudget(req, res, next) {
+  db.oneOrNone('UPDATE BudgetCategory SET monthlydollaramount=${body.monthlydollaramount} WHERE month_=${month} AND year_=${year} RETURNING id', req.body)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+
+// Creates new subcategory with dollar amount for a particular category in the month budget for a User
+function createSubcategory(req, res, next) {
+  db.one('INSERT INTO BudgetSubcategory(budgetcategoryID, subcategoryname, monthlydollaramount) VALUES (${budgetcategoryID}, ${subcategoryname}, ${monthlydollaramount} RETURNING id', req.body)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+
+// Updates the name of Subcategory
+function updateSubcategoryName(req, res, next) {
+  db.oneOrNone('UPDATE BudgetSubcategory SET subcategoryname=${body.subcategoryname} WHERE ID=${ID} RETURNING id', req.params)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+// Updates the monthly dollar amount of Subcategory
+function updateSubcategoryAmount(req, res, next) {
+  db.oneOrNone('UPDATE BudgetSubcategory SET monthlydollaramount=${body.monthlydollaramount} WHERE ID=${ID} RETURNING id', req.params)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+
+// Deletes subcategory
+function deleteSubcategory(req, res, next) {
+  db.oneOrNone('DELETE FROM BudgetSubcategory WHERE ID=${ID} RETURNING id', req.params)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+
 
 
 
